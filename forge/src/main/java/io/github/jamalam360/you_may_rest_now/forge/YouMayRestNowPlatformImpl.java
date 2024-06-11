2a77967763cd61c873e22117faae89aed465f367
James (Jamalam) <james@jamalam.tech>
2024-06-11 17:54:26 +0100

    feat: update to 1.20.6

--- forge/src/main/java/io/github/jamalam360/you_may_rest_now/forge/YouMayRestNowPlatformImpl.java
+++
@@ -1,7 +0,0 @@
-package io.github.jamalam360.you_may_rest_now.forge;
-
-import io.github.jamalam360.you_may_rest_now.YouMayRestNowPlatform;
-
-public class YouMayRestNowPlatformImpl {
-	
-}
--- quilt/src/main/java/io/github/jamalam360/you_may_rest_now/fabric/YouMayRestNowPlatformImpl.java
+++
@@ -1,7 +0,0 @@
-package io.github.jamalam360.you_may_rest_now.fabric;
-
-import io.github.jamalam360.you_may_rest_now.YouMayRestNowPlatform;
-
-public class YouMayRestNowPlatformImpl {
-	
-}
